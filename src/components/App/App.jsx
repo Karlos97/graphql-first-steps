15d6f66ecd587fa1720e65230978206ef124b01a
Karlos97 <k.minta97@gmail.com>
2022-02-05 08:31:13 -0800

    search f.e "Prisma"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import CreateLink from '../CreateLink/CreateLink';
 import Header from '../Header/Header';
 import LinkList from '../LinkList/LinkList';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../Login/Login';
+import Search from '../Search/Search';
 
-const App = () => {
-  return (
-    <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Routes>
-          <Route path="/" element={<LinkList />} />
-          <Route path="/create" element={<CreateLink />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/new/1" />} />
+
+        <Route path="/create" element={<CreateLink />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
